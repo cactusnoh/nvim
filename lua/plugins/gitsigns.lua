960c88ae4e79aba69b5970e10a8ed8bba4660130
Jeong Yeon Noh <cactusnoh@gmail.com>
2026-03-16 06:45:18 +0000

    Add some keymaps to gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,5 +2,9 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     lazy = false,
+    keys = {
+	{ "<leader>gb", "<cmd>Gitsigns blame<cr>", desc = "Gitsigns blame" },
+	{ "<leader>gh", "<cmd>Gitsigns preview_hunk<cr>", desc = "Gitsigns preview_hunk" },
+    },
   }
 }
